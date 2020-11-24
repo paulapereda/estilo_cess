44af20df86d07c529781e860ad90a5fa44eac580
Paula Pereda <paaupereda@gmail.com>
2020-11-24 16:18:03 -0300

    población

--- Notas Técnicas/01_Población/viz-poblacion.R
+++ Notas Técnicas/01_Población/viz-poblacion.R
@@ -1,8 +1,8 @@
-library(lubridate)
 library(patchwork)
 library(tidyverse)
 library(scales)
 library(readxl)
+
 source(here::here("estilo_cess.R"))
 
 # Gráfico 1 - Natalidad y Tasa Global de Fecundidad
@@ -13,7 +13,7 @@ g1 <- read_excel("Notas Técnicas/01_Población/insumos graficos Nota tecnica 
             Nacimientos = Nacimientos,
             TGF = TGF) %>% 
   pivot_longer(- anio, names_to = "variable", values_to = "valor") %>% 
-  mutate(label = ifelse(valor < 3, round(valor, 2), format(round(valor), big.mark = ".")))
+  mutate(label = ifelse(valor < 3, format(round(valor, 2), decimal.mark = ","), format(round(valor), big.mark = ".")))
 
 nacimiento_labels <- filter(g1 %>% filter(variable == "Nacimientos"), 
                               anio %in% c(1996, 1999, 2002, 2005, 2008, 2011,
@@ -23,21 +23,25 @@ nacimiento_labels <- filter(g1 %>% filter(variable == "Nacimientos"),
   filter(variable == "Nacimientos") %>% 
    ggplot(aes(anio, valor)) +
     geom_line(color = verde_cess, size = 1.1) +
-    scale_x_continuous(limits = c(1996, 2019),
-                       breaks = c(1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
-                                2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013,
-                                2014, 2015, 2016, 2017, 2018, 2019)) +
-    scale_y_continuous(limits = c(0, 60000),
-                     breaks = c(0, 20000, 40000, 60000),
-                     labels = c("0", "20.000", "40.000", "60.000")) +
+    geom_point(data = nacimiento_labels, color = verde_cess, size = 2) +
+    scale_x_continuous(breaks = c(1996, 1999, 2002, 2005, 2008, 2011, 2014, 2017, 2020),
+                       expand = expansion(mult = c(0.04, 0.06))) +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.05)),
+                       limits = c(0, 65000),
+                       breaks = c(0, 20000, 40000, 60000),
+                       labels = c("0", "20.000", "40.000", "60.000")) +
   geom_text(data = nacimiento_labels, 
-            nudge_y = .6e-2,
+            nudge_y = 2700,
             size = 5,
             aes(label = label)) + 
   labs(x = "",
        y = "",
        subtitle = "Natalidad") + 
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)))
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          axis.line.x = element_line(),
+          axis.ticks.x = element_line(),
+          legend.position = "bottom"))
 
 tgf_labels <- filter(g1 %>% filter(variable == "TGF"), 
                             anio %in% c(1996, 1999, 2002, 2005, 2008, 2011,
@@ -45,21 +49,27 @@ tgf_labels <- filter(g1 %>% filter(variable == "TGF"),
 (tgf <- g1 %>% 
   filter(variable == "TGF") %>% 
   ggplot(aes(anio, valor)) +
+  geom_point(data = tgf_labels, color = verde_cess, size = 2) +
   geom_line(color = verde_cess, size = 1.1) +
-  scale_x_continuous(limits = c(1996, 2019),
-                     breaks = c(1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
-                                2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013,
-                                2014, 2015, 2016, 2017, 2018, 2019)) +
-  scale_y_continuous(limits = c(0, 2.5)) +
+    scale_x_continuous(breaks = c(1996, 1999, 2002, 2005, 2008, 2011, 2014, 2017, 2020),
+                       expand = expansion(mult = c(0.03, 0.06))) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.08)),
+                     limits = c(0, 2.7),
+                     breaks = c(0, .5, 1, 1.5, 2, 2.5),
+                     labels = c(0, "0,5", "1,0", "1,5", "2,0", "2,5")) +
   geom_text(data = tgf_labels, 
-            nudge_y = .6e-2,
+            nudge_y = 0.13,
             size = 5,
             aes(label = label)) + 
   labs(x = "",
        y = "",
        subtitle = "Tasa Global de Fecundidad",
        caption = "Fuente: Elaboración en base a estadísticas vitales (MSP)") + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)))
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          axis.line.x = element_line(),
+          axis.ticks.x = element_line(),
+          legend.position = "bottom"))
 
 nacimientos + tgf + 
   plot_layout(ncol = 1) + 
@@ -74,12 +84,7 @@ g3 <- read_excel("Notas Técnicas/01_Población/insumos graficos Nota tecnica 
   rename(sexo = "E(0)") %>% 
   mutate(sexo = str_to_title(sexo)) %>% 
   pivot_longer(- sexo, names_to = "anio", values_to = "valor") %>% 
-  mutate(label = paste(valor, "%", sep = ""))
-
-
-evn_labels <- filter(g2, anio %in% c("2020-2025", "2095-2100"))
-
-g3 %>% 
+  mutate(label = paste(format(valor, decimal.mark = ","), "%", sep = "")) %>% 
   mutate(proyeccion = list("1950-1955" = F,  "1955-1960" = F,  "1960-1965" = F, 
                            "1965-1970" = F,  "1970-1975" = F,  "1975-1980" = F, 
                            "1980-1985" = F,  "1985-1990" = F,  "1990-1995" = F, 
@@ -90,14 +95,19 @@ g3 %>%
                            "2055-2060" = T,  "2060-2065" = T,  "2065-2070" = T, 
                            "2070-2075" = T,  "2075-2080" = T,  "2080-2085" = T, 
                            "2085-2090" = T,  "2090-2095" = T,  "2095-2100" = T)[anio]) %>% 
-  unnest(proyeccion) %>% 
+  unnest(proyeccion)
+
+evn_labels <- filter(g3, anio %in% c("2020-2025", "2095-2100"))
+
+g3 %>% 
   ggplot(aes(anio, valor, color = sexo, group = interaction(sexo, proyeccion))) +
   geom_line(aes(linetype = proyeccion), size = 1.1) +
   scale_color_manual(values = c(verde_cess, violeta_cess)) +
+  scale_x_discrete(expand = expansion(mult = c(0.03, 0.06))) +
   scale_y_continuous(limits = c(0, 100)) +
   geom_text(data = evn_labels, 
             nudge_y = .6e-2,
-            size = 5,
+            size = 4.7,
             vjust = -.25,
             hjust = .65,
             aes(label = label),
@@ -109,6 +119,10 @@ g3 %>%
        caption = "Fuente: WPP - Naciones Unidas. Rev. 2019") +
   guides(linetype = FALSE) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.line.x = element_line(),
+        axis.ticks.x = element_line(),
         legend.position = "bottom") +
   ggsave(here::here("Notas Técnicas", "01_Población", "plots", "graf3_esperanza_vida.png"), dpi = 300, width = 11, height = 7)
 
@@ -123,11 +137,14 @@ polacion_labels <- filter(g4, anio %in% c(2020, 2040, 2060, 2080, 2100))
   
 g4 %>% 
   ggplot(aes(anio, Total)) +
-  geom_col(position = "dodge", fill = verde_cess) +
-  scale_x_continuous(breaks = c(2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090, 2100)) +
-  scale_y_continuous(breaks = c(0, 500000, 1000000, 1500000, 2000000, 2500000, 3000000, 3500000),
+  geom_col(position = "dodge", fill = verde_cess, color = "black") +
+  scale_x_continuous(expand = expansion(mult = c(0.03, 0.06)),
+                     breaks = c(2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090, 2100)) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.08)),
+                     limits = c(0, 4000000),
+                     breaks = c(0, 500000, 1000000, 1500000, 2000000, 2500000, 3000000, 3500000, 4000000),
                      labels = c("0", "500.000", "1.000.000", "1.500.000", "2.000.000", "2.500.000", 
-                                "3.000.000", "3.500.000")) +
+                                "3.000.000", "3.500.000", "4.000.000")) +
   geom_text(data = polacion_labels, 
             position = position_dodge(1),
             vjust = -0.25,
@@ -137,6 +154,10 @@ g4 %>%
        y = "",
        title = "Gráfico 4 - Proyección de población total",
        caption = "Fuente: elaboración propia") +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.line.x = element_line(),
+        axis.ticks.x = element_line()) +
   ggsave(here::here("Notas Técnicas", "01_Población", "plots", "graf4_proyeccion_poblacion.png"), dpi = 300, width = 11, height = 7)
 
 # Gráfico 5 - Proyección de población por sexo
@@ -150,9 +171,12 @@ g5 <- read_excel("Notas Técnicas/01_Población/insumos graficos Nota tecnica 
 
 g5 %>% 
   ggplot(aes(anio, valor, group = sexo)) +
-  geom_col(aes(fill = sexo), position = "dodge") +
-  scale_x_continuous(breaks = c(2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090, 2100)) +
-  scale_y_continuous(breaks = c(0, 500000, 1000000, 1500000, 2000000),
+  geom_col(aes(fill = sexo), position = "dodge", color = "black") +
+  scale_x_continuous(expand = expansion(mult = c(0.03, 0.06)),
+                     breaks = c(2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090, 2100)) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.08)),
+                     limits = c(0, 2000000),
+                     breaks = c(0, 500000, 1000000, 1500000, 2000000),
                      labels = c("0", "500.000", "1.000.000", "1.500.000", "2.000.000")) +
   scale_fill_manual(values = c(verde_cess, violeta_cess)) +
   labs(x = "",
@@ -160,7 +184,11 @@ g5 %>%
        fill = "",
        title = "Gráfico 5 - Proyección de población por sexo",
        caption = "Fuente: elaboración propia") +
-  theme(legend.position = "bottom") +
+  theme(legend.position = "bottom",
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.line.x = element_line(),
+        axis.ticks.x = element_line()) +
   ggsave(here::here("Notas Técnicas", "01_Población", "plots", "graf5_proyeccion_sexo_poblacion.png"), dpi = 300, width = 11, height = 7)
 
 # Gráfico 6 - Proyección de población por sexo y edad
@@ -172,7 +200,7 @@ g6 <- read_excel("Notas Técnicas/01_Población/insumos graficos Nota tecnica 
 
 g6 %>% 
   ggplot(aes(anio, valor, group = grupoedad)) +
-  geom_col(aes(fill = grupoedad), position = "dodge") +
+  geom_col(aes(fill = grupoedad), position = "dodge", color = "black") +
   scale_y_continuous(breaks = c(0, 0.2, 0.4, 0.6, 0.8),
                      labels = c(0, 20, 40, 60, 80)) +
   scale_fill_manual(values = c(amarillo_cess, verde_cess, violeta_cess)) +
@@ -190,11 +218,31 @@ g6 %>%
   ggsave(here::here("Notas Técnicas", "01_Población", "plots", "graf6_proyeccion_sexo_edad_poblacion.png"), dpi = 300, width = 11, height = 7)
 
 # Gráfico 7 - Pirámide de población
-# 
-# g7 <- read_excel("Notas Técnicas/01_Población/insumos graficos Nota tecnica 1.xlsx", 
-#                  sheet = "gráfico 7", range = "P1:T22") %>% 
-#   mutate(edad = fct_inorder(grupoedad))  %>% 
-#   select( - grupoedad) %>% 
-#   pivot_longer(- edad) %>% 
-#   separate(name, into = c("sexo", "year")) %>% 
-  
+
+g7 <- read_excel("Notas Técnicas/01_Población/insumos graficos Nota tecnica 1.xlsx",
+                 sheet = "gráfico 7", range = "P1:T22") %>%
+  mutate(edad = fct_inorder(grupoedad))  %>%
+  select( - grupoedad) %>%
+  pivot_longer(- edad) %>%
+  mutate(value = value*100) %>% 
+  separate(name, into = c("sexo", "year"))
+
+g7 %>% 
+  ggplot(aes(edad, value)) +
+  geom_col(aes(fill = sexo), color = "black") +
+  coord_flip() +
+  scale_fill_manual(values = c(verde_cess, violeta_cess), 
+                    name = "") +
+  scale_y_continuous(expand = expansion(add = c(1, 1)),
+                     breaks = c(-4, -3, -2, -1, 0, 1, 2, 3, 4)) +
+  facet_wrap(~ year, nrow = 1) +
+  labs(x = "",
+       y = "",
+       caption = "Fuente: elaboración propia",
+       title = "Gráfico 7 - Pirámide de población") + 
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.line.x = element_line(),
+        axis.ticks.x = element_line(),
+        legend.position = "bottom") +
+  ggsave(here::here("Notas Técnicas", "01_Población", "plots", "graf7_piramide.png"), dpi = 300, width = 10, height = 6)
